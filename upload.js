5df4d7f22fed74e03c79816afe8179e1e9c07eaf
unknown <a5c5yzz@W01165865.usac.mmm.com>
2015-09-16 12:37:59 -0500

    first commit

---
+++ upload.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+
+    var app = angular.module('app', [
+        'ngAnimate',
+        'ngRoute',
+        'ui.bootstrap',
+        'textAngular',
+        'ngFileUpload',
+        'akoenig.deckgrid',
+        'pdf'
+    ]);
+})();
